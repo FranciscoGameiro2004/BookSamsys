359bc27c611986dc886f55349a70b4058aa80fa0
Francisco Gameiro <gameirofrancisco6@gmail.com>
2025-03-17 09:26:33 +0000

    build: :package: Instalação do *package* `React Router` e adaptação de ficheiros

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import './css/index.css'
+import Dashboard from './views/Dashboard.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/views/Dashboard.tsx
@@ -0,0 +1,14 @@
+import '../css/Dashboard.css'
+
+function Dashboard() {
+
+  return (
+    <>
+      <table border={1}>
+        
+      </table>
+    </>
+  )
+}
+
+export default Dashboard
