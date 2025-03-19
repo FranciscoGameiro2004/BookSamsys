7bc05e6b6cd80077e2fe40e9af01a755e1719745
Francisco Gameiro <gameirofrancisco6@gmail.com>
2025-03-19 10:45:24 +0000

    refactor: :lipstick: Mudança dos botões utilizados

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,112 +1,149 @@
-import { useState, useEffect, ChangeEventHandler, FormEventHandler } from "react";
+import {
+  useState,
+  useEffect,
+  ChangeEventHandler,
+  FormEventHandler,
+} from "react";
 import "../css/Dashboard.css";
 
 import { type Book } from "../types/books";
+
+import { Button, IconButton } from "@mui/material";
+import { NavigateNext, NavigateBefore } from "@mui/icons-material";
 
 function Dashboard() {
   const apiURL = import.meta.env.VITE_API_BASE_URL;
 
   const [booksList, setBooksList] = useState<Book[]>([]);
   const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(1)
-  const [quantityPerPage, setQuantityPerPage] = useState(15)
+  const [page, setPage] = useState(1);
+  const [quantityPerPage, setQuantityPerPage] = useState(15);
 
-  const [search, setSearch] = useState('')
-  const [orderBy, setOrderBy] = useState('')
-  const [sortBy, setSortBy] = useState('')
-  const [orderAndSort, setOrderAndSort] = useState('')
+  const [search, setSearch] = useState("");
+  const [orderBy, setOrderBy] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [orderAndSort, setOrderAndSort] = useState("");
 
-  const [genreFilter, setGenreFilter] = useState('')
+  const [genreFilter, setGenreFilter] = useState("");
 
-  const [minPrice, setMinPrice] = useState(0)
-  const [maxPrice, setMaxPrice] = useState(1000)
-  const [priceRange, setPriceRange] = useState(`&price_gte=${minPrice}&price_lte=${maxPrice}`)
-  const [minRating, setMinRating] = useState(1)
-  const [onlyAvailable, setOnlyAvailable] = useState(false)
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(1000);
+  const [priceRange, setPriceRange] = useState(
+    `&price_gte=${minPrice}&price_lte=${maxPrice}`
+  );
+  const [minRating, setMinRating] = useState(1);
+  const [onlyAvailable, setOnlyAvailable] = useState(false);
 
   useEffect(() => {
-    fetchBooks()
+    fetchBooks();
   }, [page, quantityPerPage]);
 
   useEffect(() => {
-    if (orderBy !== '' && sortBy !== '') {
-      setOrderAndSort(`&sort=${sortBy}&order=${orderBy}`)
-    } else if (sortBy !== '') {
-      setOrderAndSort(`&sort=${sortBy}`)
+    if (orderBy !== "" && sortBy !== "") {
+      setOrderAndSort(`&sort=${sortBy}&order=${orderBy}`);
+    } else if (sortBy !== "") {
+      setOrderAndSort(`&sort=${sortBy}`);
     } else {
-      setOrderAndSort('')
+      setOrderAndSort("");
     }
-  }, [orderBy, sortBy])
+  }, [orderBy, sortBy]);
 
   useEffect(() => {
-    setPriceRange(`&price_gte=${minPrice}&price_lte=${maxPrice}`)
-  }, [minPrice, maxPrice])
+    setPriceRange(`&price_gte=${minPrice}&price_lte=${maxPrice}`);
+  }, [minPrice, maxPrice]);
 
-  const fetchBooks = async ():Promise<void> => {
+  const fetchBooks = async (): Promise<void> => {
     setLoading(true);
-    fetch(apiURL + "books" + `?page=${page}` + `&limit=${quantityPerPage}` + `&search=${search}` + orderAndSort + genreFilter + priceRange + `&rating_gte=${minRating}` + (onlyAvailable ? `&available_eq=${onlyAvailable}`: ''))
+    fetch(
+      apiURL +
+        "books" +
+        `?page=${page}` +
+        `&limit=${quantityPerPage}` +
+        `&search=${search}` +
+        orderAndSort +
+        genreFilter +
+        priceRange +
+        `&rating_gte=${minRating}` +
+        (onlyAvailable ? `&available_eq=${onlyAvailable}` : "")
+    )
       .then((res) => res.json())
       .then((json) => {
         setBooksList(json);
         console.log(json);
       });
     setLoading(false);
-  } 
+  };
 
-  const handleClickPage = (input: 'next' | 'previous'): void => {
-    if (input === 'next') {
-      setPage(previousPage => previousPage + 1)
-    } else if (input === 'previous') {
-      setPage(previousPage => previousPage - 1)
+  const handleClickPage = (input: "next" | "previous"): void => {
+    if (input === "next") {
+      setPage((previousPage) => previousPage + 1);
+    } else if (input === "previous") {
+      setPage((previousPage) => previousPage - 1);
     }
-  }
+  };
 
   const handleSearchChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setSearch(e.target.value)
-  }
+    setSearch(e.target.value);
+  };
 
   const handleFormSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault()
-    setPage(1)
-    await fetchBooks()
-  }
+    e.preventDefault();
+    setPage(1);
+    await fetchBooks();
+  };
 
   const handleSortChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
-    setSortBy(e.target.value)
-  }
+    setSortBy(e.target.value);
+  };
 
-  const handleOrderChange = (input: 'asc' | 'desc') => {
-    setOrderBy(input)
-  }
+  const handleOrderChange = (input: "asc" | "desc") => {
+    setOrderBy(input);
+  };
 
-  const handleGenreFilterChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
-    setGenreFilter(e.target.value)
-  }
+  const handleGenreFilterChange: ChangeEventHandler<HTMLSelectElement> = (
+    e
+  ) => {
+    setGenreFilter(e.target.value);
+  };
 
   const handleMinPriceChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setMinPrice(+e.target.value)
-  }
+    setMinPrice(+e.target.value);
+  };
   const handleMaxPriceChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setMaxPrice(+e.target.value)
-  }
+    setMaxPrice(+e.target.value);
+  };
 
   const handleMinRatingChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setMinRating(+e.target.value)
-  }
+    setMinRating(+e.target.value);
+  };
 
-  const handleOnlyAvailableChange: ChangeEventHandler<HTMLInputElement> = () => {
-    setOnlyAvailable(previousValue => !previousValue)
-  }
+  const handleOnlyAvailableChange: ChangeEventHandler<
+    HTMLInputElement
+  > = () => {
+    setOnlyAvailable((previousValue) => !previousValue);
+  };
 
   return (
     <>
       <h1>Livros</h1>
       <div>
         <form action="#" onSubmit={handleFormSubmit}>
-          <input type="text" value={search} placeholder="Pesquisar" name="search" id="search" onChange={handleSearchChange}/>
+          <input
+            type="text"
+            value={search}
+            placeholder="Pesquisar"
+            name="search"
+            id="search"
+            onChange={handleSearchChange}
+          />
           <div>
             <label htmlFor="sortBy">Ordenar por:</label>
-            <select name="sortBy" id="sortBy" value={sortBy} onChange={handleSortChange}>
+            <select
+              name="sortBy"
+              id="sortBy"
+              value={sortBy}
+              onChange={handleSortChange}
+            >
               <option value="">Selecione</option>
               <option value="title">Nome</option>
               <option value="author">Autor</option>
@@ -117,63 +154,117 @@ function Dashboard() {
           <div>
             <label>Por ordem:</label>
             <div>
-              <input type="radio" name="orderBy" id="ascRadioBtn" value="asc" onClick={() => handleOrderChange('asc')} />
+              <input
+                type="radio"
+                name="orderBy"
+                id="ascRadioBtn"
+                value="asc"
+                onClick={() => handleOrderChange("asc")}
+              />
               <label htmlFor="ascRadioBtn">Crescente</label>
-              <input type="radio" name="orderBy" id="descRadioBtn" value="desc" onClick={() => handleOrderChange('desc')} />
+              <input
+                type="radio"
+                name="orderBy"
+                id="descRadioBtn"
+                value="desc"
+                onClick={() => handleOrderChange("desc")}
+              />
               <label htmlFor="descRadioBtn">Derescente</label>
             </div>
           </div>
           <div>
             <label htmlFor="">Filtros</label>
-            {
-              /*Filtrar por
+            {/*Filtrar por
                 [X] Gênero (opt)
                 [X] Preço (min + max)
                 [X] Avaliação (min)
                 [] Disponibilidade (t/f)
-              */
-            }
+              */}
             <div>
-            <label htmlFor="sortBy">Gênero:</label>
-            <select name="genreFilter" id="genreFilter" value={genreFilter} onChange={handleGenreFilterChange}>
-              <option value="">Selecione</option>
-              <option value="&genre_eq=Fantasy">Fantasy</option>
-              <option value="&genre_eq=Science Fiction">Science Fiction</option>
-              <option value="&genre_eq=Comic">Comic</option>
-              <option value="&genre_eq=Poetry">Poetry</option>
-              <option value="&genre_eq=Children's Literature">Children's Literature</option>
-              <option value="&genre_eq=Adventure">Adventure</option>
-              <option value="&genre_eq=Psychology">Psychology</option>
-              <option value="&genre_eq=Business">Business</option>
-              <option value="&genre_eq=Classic">Classic</option>
-              <option value="&genre_eq=Romance">Romance</option>
-              <option value="&genre_eq=Comedy">Comedy</option>
-              <option value="&genre_eq=Thriller">Thriller</option>
-              <option value="&genre_eq=Historical Fiction">Historical Fiction</option>
-              <option value="&genre_eq=Western">Western</option>
-              <option value="&genre_eq=Religion">Religion</option>
-              <option value="&genre_eq=Mythology">Mythology</option>
-              <option value="&genre_eq=Philosophy">Philosophy</option>
-              <option value="&genre_eq=Biography">Biography</option>
-            </select>
+              <label htmlFor="sortBy">Gênero:</label>
+              <select
+                name="genreFilter"
+                id="genreFilter"
+                value={genreFilter}
+                onChange={handleGenreFilterChange}
+              >
+                <option value="">Selecione</option>
+                <option value="&genre_eq=Fantasy">Fantasy</option>
+                <option value="&genre_eq=Science Fiction">
+                  Science Fiction
+                </option>
+                <option value="&genre_eq=Comic">Comic</option>
+                <option value="&genre_eq=Poetry">Poetry</option>
+                <option value="&genre_eq=Children's Literature">
+                  Children's Literature
+                </option>
+                <option value="&genre_eq=Adventure">Adventure</option>
+                <option value="&genre_eq=Psychology">Psychology</option>
+                <option value="&genre_eq=Business">Business</option>
+                <option value="&genre_eq=Classic">Classic</option>
+                <option value="&genre_eq=Romance">Romance</option>
+                <option value="&genre_eq=Comedy">Comedy</option>
+                <option value="&genre_eq=Thriller">Thriller</option>
+                <option value="&genre_eq=Historical Fiction">
+                  Historical Fiction
+                </option>
+                <option value="&genre_eq=Western">Western</option>
+                <option value="&genre_eq=Religion">Religion</option>
+                <option value="&genre_eq=Mythology">Mythology</option>
+                <option value="&genre_eq=Philosophy">Philosophy</option>
+                <option value="&genre_eq=Biography">Biography</option>
+              </select>
             </div>
             <div>
-              <input type="range" name="minPrice" min={0} max={maxPrice} value={minPrice} id="minPrice" onChange={handleMinPriceChange} />
-              <input type="range" name="maxPrice" min={minPrice} max={3000} value={maxPrice} id="maxPrice" onChange={handleMaxPriceChange}/>
-              <p>Min: {minPrice}€ | Max: {maxPrice}€</p>
+              <input
+                type="range"
+                name="minPrice"
+                min={0}
+                max={maxPrice}
+                value={minPrice}
+                id="minPrice"
+                onChange={handleMinPriceChange}
+              />
+              <input
+                type="range"
+                name="maxPrice"
+                min={minPrice}
+                max={3000}
+                value={maxPrice}
+                id="maxPrice"
+                onChange={handleMaxPriceChange}
+              />
+              <p>
+                Min: {minPrice}€ | Max: {maxPrice}€
+              </p>
             </div>
             <div>
-              <label >Nota Mínima: {minRating}★</label>
+              <label>Nota Mínima: {minRating}★</label>
               <br />
-              <input type="range" name="maxPrice" min={1} max={5} value={minRating} id="minRating" onChange={handleMinRatingChange}/>
+              <input
+                type="range"
+                name="maxPrice"
+                min={1}
+                max={5}
+                value={minRating}
+                id="minRating"
+                onChange={handleMinRatingChange}
+              />
             </div>
           </div>
           <div>
             <label htmlFor="onlyAvailable">Somente os disponíveis? </label>
-            <input type="checkbox" name="onlyAvailable" id="onlyAvailable" onChange={handleOnlyAvailableChange} />
+            <input
+              type="checkbox"
+              name="onlyAvailable"
+              id="onlyAvailable"
+              onChange={handleOnlyAvailableChange}
+            />
           </div>
           <br />
-          <input type="submit" value="Procurar" />
+          <Button type="submit" variant="contained">
+            Procurar
+          </Button>
         </form>
       </div>
       <hr />
@@ -191,27 +282,43 @@ function Dashboard() {
           </tr>
         </thead>
         <tbody>
-          {booksList.map((book) => 
-            (
-              <tr key={book.uuid}>
-                <td>{book.isbn}</td>
-                <td>{book.title}</td>
-                <td>{book.author}</td>
-                <td>{book.publisher}</td>
-                <td>{book.genre}</td>
-                <td>{book.price}€</td>
-                <td>{book.rating}/5</td>
-                <td>{book.available ? "Em Stock" : "Fora de Stock"}</td>
-              </tr>
-            )
-          )}
+          {booksList.map((book) => (
+            <tr key={book.uuid}>
+              <td>{book.isbn}</td>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.publisher}</td>
+              <td>{book.genre}</td>
+              <td>{book.price}€</td>
+              <td>{book.rating}/5</td>
+              <td>{book.available ? "Em Stock" : "Fora de Stock"}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <div>
-        {page > 1 && <button type="button" onClick={() => handleClickPage('previous')}>{'<'}</button>}
+        {page > 1 && (
+          <IconButton
+            onClick={() => handleClickPage("previous")}
+            aria-label="anterior"
+          >
+            <NavigateBefore />
+          </IconButton>
+        )}
         <label>Page {page}</label>
-        {//! Não consigo buscar limite da paginação pela API
-        page < Math.floor(100/quantityPerPage) + (100%quantityPerPage > 0 ? 1 : 0) && <button type="button"  onClick={() => handleClickPage('next')}>{'>'}</button>}
+        {
+          //! Não consigo buscar limite da paginação pela API
+          page <
+            Math.floor(100 / quantityPerPage) +
+              (100 % quantityPerPage > 0 ? 1 : 0) && (
+            <IconButton
+              onClick={() => handleClickPage("next")}
+              aria-label="anterior"
+            >
+              <NavigateNext />
+            </IconButton>
+          )
+        }
       </div>
     </>
   );
