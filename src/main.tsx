7fb22e27ceef4bebb1e7b3739b180879d25c4565
Francisco Gameiro <gameirofrancisco6@gmail.com>
2025-03-20 12:28:38 +0000

    feat: :sparkles: Criação de página de login e e implementação de restrições de rotas (a ser melhorado posteriormente)

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 import './css/index.css'
 import Dashboard from './views/Dashboard.tsx'
+import LoginPage from './views/LoginPage.tsx'
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+function DashboardRoute() {
+  if (true) {
+    return <Dashboard />
+  } else {
+    return <Navigate to="/login" replace />
+  }
+}
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<DashboardRoute />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
