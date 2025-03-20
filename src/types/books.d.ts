16bc2ebc578f191fb0377ee26efba72bacea4fb6
Francisco Gameiro <gameirofrancisco6@gmail.com>
2025-03-20 18:32:07 +0000

    refactor: :sparkles: Adição do tipo author e adaptações do tipo book

--- src/types/books.d.ts
+++ src/types/books.d.ts
@@ -1,8 +1,11 @@
+import { Author } from "./authors";
+
 export interface Book {
     uuid: string,
     isbn: string,
     title: string,
     author: string,
+    authorId: Author.uuid | string,
     publisher: string,
     genre: string,
     price: number,
