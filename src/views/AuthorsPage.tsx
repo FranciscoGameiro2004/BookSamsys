7824f616b85100c584595f9b4dc7318d096bc20f
Francisco Gameiro <gameirofrancisco6@gmail.com>
2025-03-24 15:07:33 +0000

    refactor: :lipstick: Estruturação da página de autores

--- src/views/AuthorsPage.tsx
+++ src/views/AuthorsPage.tsx
@@ -1,5 +1,65 @@
+import {
+  List,
+  ListItem,
+  ListItemText,
+  IconButton,
+  Box,
+  TextField,
+} from "@mui/material";
+import { Edit, Delete } from "@mui/icons-material";
+
+import type { Author } from "../types/authors";
+
+import { useEffect, useState } from "react";
+
 export default function AuthorsPage() {
-    return (
-        <h1>OK</h1>
-    )
+  const apiURL = import.meta.env.VITE_API_BASE_URL;
+
+  const [authorsList, setAuthorsList] = useState<Author[]>([]);
+
+  const fetchAuthors = async (): Promise<void> => {
+    fetch(apiURL + "authors")
+      .then((res) => res.json())
+      .then((json) => {
+        setAuthorsList(json);
+        console.log(json);
+      });
+  };
+
+  useEffect(() => {
+    fetchAuthors();
+  }, []);
+
+  return (
+    <>
+      <h1>Autores</h1>
+      <Box
+        sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}
+      >
+        <TextField sx={{ width: "100%", maxWidth: 500 }} label="Pesquisa" />
+        <List
+          sx={{ width: "100%", maxWidth: 500, bgcolor: "background.paper" }}
+        >
+          {authorsList.map((author) => (
+            <ListItem
+              key={author.uuid}
+              disableGutters
+              secondaryAction={
+                <>
+                  <IconButton aria-label="editar">
+                  <Edit />
+                  </IconButton>
+                  <IconButton aria-label="apagar">
+                  <Delete />
+                  </IconButton>
+                </>
+              }
+            >
+              <ListItemText primary={author.author} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </>
+  );
 }
