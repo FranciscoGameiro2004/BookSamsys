c12d7e0248a27a9c2e54a6d6db4b2d225e4abde7
Francisco Gameiro <gameirofrancisco6@gmail.com>
2025-03-24 15:37:38 +0000

    feat: :sparkles: Implementar funcionalidade de apagar autores

--- src/views/AuthorsPage.tsx
+++ src/views/AuthorsPage.tsx
@@ -5,26 +5,60 @@ import {
   IconButton,
   Box,
   TextField,
+  Fab,
+  Modal,
+  Typography,
+  Button
 } from "@mui/material";
-import { Edit, Delete } from "@mui/icons-material";
+import { Edit, Delete, Add } from "@mui/icons-material";
 
 import type { Author } from "../types/authors";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, FormEventHandler } from "react";
 
 export default function AuthorsPage() {
   const apiURL = import.meta.env.VITE_API_BASE_URL;
 
   const [authorsList, setAuthorsList] = useState<Author[]>([]);
+  const [loading, setLoading] = useState(false)
+
+  const [selectedAuthor, setSelectedAuthor] = useState<Author>()
+
+  const [openAddEditAuthorModal, setOpenAddEditAuthorModal] = useState(false);
+  const [openDeleteAuthorModal, setOpenDeleteAuthorModal] = useState(false);
 
   const fetchAuthors = async (): Promise<void> => {
-    fetch(apiURL + "authors")
+    setLoading(true)
+    await fetch(apiURL + "authors")
       .then((res) => res.json())
       .then((json) => {
         setAuthorsList(json);
         console.log(json);
       });
+    setLoading(false)
   };
+
+  const handleOpenDeleteAuthorModal = (author: Author) => {
+    setSelectedAuthor(author)
+    setOpenDeleteAuthorModal(true)
+  }
+
+  const handleCloseDeleteAuthorModal = () => {
+    setOpenDeleteAuthorModal(false)
+  }
+
+  const handleDeleteAuthorSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    try {
+      await fetch(apiURL + "authors/" + selectedAuthor?.uuid, {
+        method: "DELETE",
+      });
+      setOpenDeleteAuthorModal(false);
+      await fetchAuthors();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   useEffect(() => {
     fetchAuthors();
@@ -32,6 +66,19 @@ export default function AuthorsPage() {
 
   return (
     <>
+      <Box>
+        <Fab
+          sx={{
+            position: "fixed",
+            bottom: 40,
+            right: 40,
+            color: "white",
+            backgroundColor: "blueviolet",
+          }}
+        >
+          <Add />
+        </Fab>
+      </Box>
       <h1>Autores</h1>
       <Box
         sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}
@@ -47,10 +94,10 @@ export default function AuthorsPage() {
               secondaryAction={
                 <>
                   <IconButton aria-label="editar">
-                  <Edit />
+                    <Edit />
                   </IconButton>
-                  <IconButton aria-label="apagar">
-                  <Delete />
+                  <IconButton onClick={() => handleOpenDeleteAuthorModal(author)} aria-label="apagar">
+                    <Delete />
                   </IconButton>
                 </>
               }
@@ -60,6 +107,36 @@ export default function AuthorsPage() {
           ))}
         </List>
       </Box>
+      <Modal open={openDeleteAuthorModal} onClose={handleCloseDeleteAuthorModal}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h5" component="h5">
+            Apagar livro?
+          </Typography>
+          <Typography id="modal-modal-body" component="p">
+            Pretende realmente apagar o autor {selectedAuthor?.author}?
+          </Typography>
+          <form action="#" onSubmit={handleDeleteAuthorSubmit}>
+            <div>
+              <Button variant="contained" type="submit">
+                Apagar
+              </Button>
+              <Button variant="outlined">Cancelar</Button>
+            </div>
+          </form>
+        </Box>
+      </Modal>
     </>
   );
 }
